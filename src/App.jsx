bf4d84c9a1a970b17b1031bfae2ee31d6cc5d57c
KC220509 <buiduccong22052003@gmail.com>
2024-06-18 01:29:55 +0700

    upload my-app-vite 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,12 @@
 
 import './App.css'
+import Login from './pages/Login';
+
+
+
 function HeaderApp(){
   return (
-      <div id="header" >
-          <div className="head-left">
-              {/* <a href="/"><img src={logo} alt=""/></a> */}
-              <span>Tốt & Nhanh</span>
-          </div>
-          <div className="head-right">
-              <div className="head-right-top">
-                  <form id="search-form" className="frm-search" method="post">
-                      <i className="ti-search"></i>
-                      <input id="input-search" className="input-search" type="text" name="key" placeholder="Giao nhanh 2H & đúng khung giờ"/>
-                      <button className="btn-search" type="submit">Tìm kiếm</button>
-                  </form>
-                  <div className="nav">
-                      <button className="btn btn-home" type="button">
-                          <i className="icon ti-home"></i>
-                          Trang chủ
-                      </button>
-                      <div className="div-nav">
-                          <button className="btn btn-account" type="button">
-                              <i className="icon ti-face-smile"></i>
-                              Tài khoản
-                          </button>
-                          <ul className="subnav">
-                              <li><a href="#">Thông tin tài khoản</a></li>
-                              <li><a href="#">Đơn hàng của tôi</a></li>
-                              <li><a href="#">Trung tâm hỗ trợ</a></li>
-                              <li><a href="#">Đăng xuất</a></li>
-                          </ul>
-                      </div>
-                      <a className="nav-cart" href="">
-                          <i className="ti-shopping-cart"></i>
-                          <span className="quantity-product-cart">0</span>
-                      </a>
-                  </div>
-              </div>
-              <div className="head-right-bottom">
-                  <i className="ti-location-pin"></i>
-                  <h4>Giao đến:</h4>
-                  <a href="">Q.Hải Châu, P.Hải Châu, Đà Nẵng</a>
-              </div>
-          </div>
-      </div>
+    <Login></Login>
   );
 }
 function App() {
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            Vui lòng đăng nhập
+        </div>
+    );
+}
+
+export default Login;
